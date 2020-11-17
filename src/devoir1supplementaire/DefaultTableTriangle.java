0f6ec06cf48749332071759b8ae0644354896720
Isaac ELBAZE <isaacelbaze@gmail.com>
2020-11-17 11:15:07 +0100

    Devoir n°1

---
+++ src/devoir1supplementaire/DefaultTableTriangle.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devoir1supplementaire;
+
+/**
+ *
+ * @author isaac
+ */
+class DefaultTableTriangle {
+
+    DefaultTableTriangle getModel() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String getText() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setText(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
